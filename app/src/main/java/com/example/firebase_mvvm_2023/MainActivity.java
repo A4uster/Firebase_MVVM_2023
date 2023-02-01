3d1f533ab6d05d4b6b1b04217796f226ad8953c4
A4uster <tdemian.9112@gmail.com>
2023-02-01 20:23:59 +0100

    PUSH conection-firebase; creacion de metodos listos para iniciar

--- app/src/main/java/com/example/firebase_mvvm_2023/MainActivity.java
+++ app/src/main/java/com/example/firebase_mvvm_2023/MainActivity.java
@@ -3,22 +3,68 @@ package com.example.firebase_mvvm_2023;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
     private EditText mETNom, mETIngreddientes,mETPreu;
-    private Button BtnAfeelir, mBtanActuaizar, mBtnEsborrar
+    private Button mBtnAñadir, mBtanActuaizar, mBtnEsborrar;
+    private FirebaseDatabase mDatabase;
+    private DatabaseReference mReference;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         InicializarComponents();
+        InicializarListeners();
     }
-    private void aianivslizarComponentes(){
+
+    private void InicializarListeners() {
+        mBtnAñadir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AñadirPizza();
+            }
+        });
+        mBtanActuaizar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ActualizarPizza();
+            }
+        });
+        mBtnEsborrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EsborrarPizza();
+            }
+        });
+    }
+
+    private void EsborrarPizza() {
+    }
+
+    private void ActualizarPizza() {
+    }
+
+    private void AñadirPizza() {
+
+    }
+
+    private void InicializarComponents(){
         mETNom = findViewById(R.id.ET_Nom);
         mETIngreddientes = findViewById(R.id.ET_Ingrediente);
         mETPreu = findViewById(R.id.ET_Precio);
+        mBtanActuaizar = findViewById(R.id.BT_Actuaizar);
+        mBtnAñadir = findViewById(R.id.BT_Añadir);
+        mBtnEsborrar = findViewById(R.id.BT_Esborrar);
+        mDatabase = FirebaseDatabase.getInstance();
+        mReference = mDatabase.getReference();
+
+
     }
 }
