6025ef543ed7261a56a77e239ca435d01a92656c
A4uster <tdemian.9112@gmail.com>
2023-02-01 19:31:47 +0100

    Segon PUSH

--- app/src/main/java/com/example/firebase_mvvm_2023/MainActivity.java
+++ app/src/main/java/com/example/firebase_mvvm_2023/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.firebase_mvvm_2023;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    private EditText mETNom, mETIngreddientes,mETPreu;
+    private Button BtnAfeelir, mBtanActuaizar, mBtnEsborrar
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        InicializarComponents();
+    }
+    private void aianivslizarComponentes(){
+        mETNom = findViewById(R.id.ET_Nom);
+        mETIngreddientes = findViewById(R.id.ET_Ingrediente);
+        mETPreu = findViewById(R.id.ET_Precio);
     }
 }
