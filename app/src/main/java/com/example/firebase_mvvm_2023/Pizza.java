a504546ea27e64be31304208d5b22160fae38672
A4uster <tdemian.9112@gmail.com>
2023-02-03 17:44:15 +0100

    Push Pizza

---
+++ app/src/main/java/com/example/firebase_mvvm_2023/Pizza.java
@@ -0,0 +1,21 @@
+package com.example.firebase_mvvm_2023;
+
+public class Pizza {
+    private String mIngredients,mNombre, mPrecio,mUid;
+    public Pizza(String nom, String ingredients, String precio, String uid){
+        mNombre = nom;
+        mIngredients = ingredients;
+        mPrecio = precio;
+        mUid = uid;
+    }
+
+    public void setmUid(String mUid) {this.mUid = mUid;}
+    public void setmPrecio(String mPrecio) {this.mPrecio = mPrecio;}
+    public void setmIngredients(String mIngredients) {this.mIngredients = mIngredients;}
+    public void setmNombre(String mNombre) {this.mNombre = mNombre;}
+
+    public String getmUid() {return mUid;}
+    public String getmPrecio() {return mPrecio;}
+    public String getmIngredients() {return mIngredients;}
+    public String getmNombre() {return mNombre;}
+}
